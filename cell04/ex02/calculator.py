2dc41f0e05b83f1a81f5b36b5a04503c37de9265
Pannatorn8811 <pantri81@gmail.com>
2025-06-24 13:48:28 +0000

    your message

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,13 @@
+print("Give me the first number: ")
+first = int(input())
+print("Give me the second number: ")
+second = int(input())
+print("Thank you!")
+plus = first + second
+minus = first - second
+mult = first * second
+div = first / second
+print(f"{first} + {second} = {plus}")
+print(f"{first} - {second} = {minus}")
+print(f"{first} / {second} = {div}")
+print(f"{first} * {second} = {mult}")
