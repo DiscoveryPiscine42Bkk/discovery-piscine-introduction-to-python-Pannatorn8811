9dd30fce324ac48798baad84feeaa2d54559589b
Pannatorn8811 <pantri81@gmail.com>
2025-06-24 08:54:52 +0000

    your message

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,15 @@
+import sys
+
+# If any argument is passed, print "none"
+if len(sys.argv) > 1:
+    print("none")
+else:
+    i = 0
+    while i <= 10:
+        print(f"Table de {i}:", end=" ")
+        j = 0
+        while j <= 10:
+            print(i * j, end=" ")
+            j += 1
+        print()  # Newline after each row
+        i += 1
