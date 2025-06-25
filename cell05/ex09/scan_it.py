6b0bf2a2d2c4c71d55b5f0a42b81447574b9cc35
Pannatorn8811 <pantri81@gmail.com>
2025-06-25 10:47:25 +0000

    your message

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -7,7 +7,7 @@ mem = len(sys.argv) - 1
 if mem == 2:
     match = re.findall(sys.argv[1], sys.argv[2])
     if match:
-        print(f"{len(match)}$")
+        print(f"{len(match)}")
     else:
         print("none")
 else:
